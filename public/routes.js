bd2cd8948d9573b8728ab1b19b5b3a873b738930
Kris Stange <kris.stange2015@gmail.com>
2016-05-15 02:45:39 -0700

    (feat) First factor D3 chart display in front-end
    package.json
    public/components/chart.js
    public/components/nav_header.js
    public/routes.js

--- public/routes.js
+++ public/routes.js
@@ -7,6 +7,7 @@ import ChatLogs from './components/chat_logs';
 import UsersList from './components/users_list';
 import User from './components/user';
 import BotActivity from './components/bot_activity';
+import Chart from './components/chart';
 import BotConfig from './components/bot_config';
 
 export default (
@@ -15,5 +16,8 @@ export default (
     <Route path='chat' component={ChatLogs} />
     <Route path='users' component={UsersList} />
     <Route path='users/:id' component={User} />
+    <Route path='activity' component={BotActivity} />
+    <Route path='chart' component={Chart} />
+    <Route path='config' component={BotConfig} />
   </Route>
 );
